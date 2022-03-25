5df4656a942631df1926446eb696765dfe3df1ff
Zak <zagladney@gmail.com>
2022-03-24 20:05:17 -0700

    calendar but numbers not showing up

---
+++ app/src/main/java/com/example/uhsapp/CalenderViewHolder.java
@@ -0,0 +1,26 @@
+package com.example.uhsapp;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CalenderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public final TextView dayOfMonth;
+    private final CalendarAdapter.OnItemListener onItemListener;
+
+    public CalenderViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener)
+    {
+        super(itemView);
+        dayOfMonth = itemView.findViewById(R.id.cellDayText);
+        this.onItemListener = onItemListener;
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        onItemListener.onItemClick(getAdapterPosition(), (String) dayOfMonth.getText());
+    }
+}
--- app/src/main/java/com/example/uhsapp/MainActivity.java
+++ app/src/main/java/com/example/uhsapp/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.uhsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener {
+
+    private LocalDate selectedDate;
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initWidgets();
+        selectedDate = LocalDate.now();
+        setMonthView();
+    }
+
+    private void setMonthView() {
+        monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonth(selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+    private ArrayList<String> daysInMonth(LocalDate date) {
+        ArrayList<String> daysInMonthList = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+
+        int daysInMonth = yearMonth.lengthOfMonth();
+
+        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+        for (int i = 1; i <= 42; i++) {
+            if (daysInMonth + dayOfWeek < i || i <= dayOfWeek) {
+                daysInMonthList.add("");
+            } else {
+                daysInMonthList.add(String.valueOf(i + dayOfWeek));
+            }
+        }
+        return daysInMonthList;
+    }
+
+    private String monthYearFromDate(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    private void initWidgets() {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+    }
+
+    public void nextMonthAction(View view) {
+        selectedDate = selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+    public void previousMonthAction(View view) {
+        selectedDate = selectedDate.minusMonths(1);
+        setMonthView();
+    }
+
+    @Override
+    public void onItemClick(int position, String dayText) {
+        if(dayText.equals("")) {
+            String output = "Selected: " + dayText + " " + monthYearFromDate(selectedDate);
+            Toast.makeText(this, output, Toast.LENGTH_LONG).show();
+        }
     }
 }
